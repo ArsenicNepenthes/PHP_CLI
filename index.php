8ef86352cdfaaa3a1ac1d68a2d27e519e6834cf6
Arsenic Népenthes <52414957+ArsenicNepenthes@users.noreply.github.com>
2019-09-24 09:16:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,41 @@
 <?php
 //ceci est un commentaire
-echo "hello world";
-echo "Bonjour le monde"
+echo "Hello world\n<br>";
+echo "Bonjour le monde\n<br>";
 /*
 commentaire 
 sur plusieurs 
 lignes
 */
+$prenom = "Cindy";
+echo $prenom;
+echo"\n";
+$age = 31;
+echo $age;
+echo "\n<br>Bonjour $prenom, vous avez $age ans.";
+echo "\n<br>".'Bonjour ' .$prenom.' , vous avez ' .$age.' ans';
+echo"\n<hr>";
+$nombre1 = 23;
+$nombre2 = 42.45;
+$resultat = 0;
+$resultat = ($nombre1 + $nombre2) * 2;
+echo $resultat;
+echo "<hr>";
+$tableau = array();
+$tableau2 = [];
+$tableau2[] = 'Cindy<br>';
+$tableau2[] = 'Pierre';
+$tableau2[] = 'Paul';
+echo $tableau2[0];
+if (empty($tableau2[0]))
+{
+    echo 'Cette entrée n\'existe pas !<br>';
+}
+else 
+{
+    echo $tableau2[0];
+}
+echo $tableau2[1];
+echo $tableau2[2];
+
 
